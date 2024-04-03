3b0d36b16ca18d6735b9358a496150ffc95c792b
JMmatito-fgi <jelamen.matito@focusglobalinc.com>
2024-04-03 11:02:21 +0800

    pull ka pls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,8 +39,7 @@ export default function Dashboard() {
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Package2 className="h-6 w-6" />
-              <span className="">Acme Inc</span>
+              <span className="">Sline</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
@@ -54,7 +53,7 @@ export default function Dashboard() {
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Home className="h-4 w-4" />
-                Dashboard
+                Home Furnishings
               </Link>
               <Link
                 href="#"
