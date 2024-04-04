ae2a04b964fe37d52c044aad5e067086da0ee13f
djaydevs <djayflores8@gmail.com>
2024-04-04 15:57:55 +0800

    chore: change bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-import { cn } from "@/lib/utils";
-import { Boxes } from "@/components/ui/background-boxes";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 export default function MainPage() {
   return (
-    <div className="h-full relative w-full overflow-hidden bg-blue-200 flex flex-col items-center justify-center">
-      <div className="absolute inset-0 w-full h-full bg-blue-500 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-      <Boxes />
-      <h1
-        className={cn(
-          "text-6xl text-slate-900 font-black tracking-wide relative z-20"
-        )}
-      >
+    <div className="h-full relative w-full overflow-hidden">
+      <BackgroundGradientAnimation>
+      <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl left-1/2 -translate-x-1/2">
+        <h1 className="bg-clip-text text-transparent drop-shadow-2xl text-6xl font-black bg-gradient-to-b from-white/80 to-white/20">
         Welcome to Sline!
-      </h1>
-      <p className="text-center text-[25px] mt-2 text-slate-900 relative z-20">
+        </h1>
+        <p className="text-center text-[25px] mt-2 text-neutral-200 relative z-20">
         Your guide to a systematic FOCUS design.
       </p>
+      </div>
+    </BackgroundGradientAnimation>
     </div>
   );
 }
